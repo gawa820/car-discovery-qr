a4c3b7586aaf55bdba882e485b13dac5987bf15a
Ogawa Taihei <74409086+gawa820@users.noreply.github.com>
2026-05-28 13:20:18 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Googleスプレッドシートを「ウェブに公開」したCSV URLを sheetCsvUrl に貼り付けてください。
 // 空欄のままなら cars.json を読み込みます。
 window.CAR_DISCOVERY_CONFIG = {
-  sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTiRnFsmQ3NHL3mltCdDRY09zlBsH5p8-oK1LZRok7fARFgZgR18Tfzhp4t0embow/pub?output=csv"
+  sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTU3ucZME3ELXezrQlhufFf31OzVJ6qOcYGHwtnr20gsMdzcnwKSvHvF-L5uMZ4pQaEJ0cUF4jwY9TB/pub?gid=639643974&single=true&output=csv"
 };
