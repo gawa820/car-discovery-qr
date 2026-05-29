8865f1893184961eaa8f2a07573dc35078bd7608
Ogawa Taihei <74409086+gawa820@users.noreply.github.com>
2026-05-29 10:52:55 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // 店舗用データ設定
 // Googleスプレッドシートを「ウェブに公開」したCSV URLを sheetCsvUrl に貼り付けてください。
-// 空欄のままなら cars.json を読み込みます。
+// appBaseUrl はQRコード生成時に使うアプリの公開URLです。
 window.CAR_DISCOVERY_CONFIG = {
-  sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTU3ucZME3ELXezrQlhufFf31OzVJ6qOcYGHwtnr20gsMdzcnwKSvHvF-L5uMZ4pQaEJ0cUF4jwY9TB/pub?gid=639643974&single=true&output=csv"
+  sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTU3ucZME3ELXezrQlhufFf31OzVJ6qOcYGHwtnr20gsMdzcnwKSvHvF-L5uMZ4pQaEJ0cUF4jwY9TB/pub?gid=639643974&single=true&output=csv",
+  appBaseUrl: "https://gawa820.github.io/car-discovery-qr/"
 };
